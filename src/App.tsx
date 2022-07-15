eadd01d39420d2ac795e43acd2e60dc95fbd3083
Fritz <danielsinaga53@gmail.com>
2022-07-15 19:20:11 +0700

    make a search filter function

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { 
+  useState,
+  useEffect
+} from 'react'
+import './App.css'
+import JSONDATA from './data.json'
 
 function App() {
+
+  const [searchTerm,setSearchTerm] = useState('')
+  const [vendorList,setVendorList] = useState([])
+  const [vendor,setVendor] = useState({})
+  const [showDetail,setShowDetail] = useState(false)
+
+  useEffect(() => {
+    let arr = []
+    JSONDATA.filter((val)=> {
+      if (searchTerm === "") {
+        arr = []
+      } else if(val.vendorName.toLowerCase().includes(searchTerm.toLowerCase())) {
+        arr.push(val)
+      }
+    })
+    setVendorList(arr)
+  }, [searchTerm])
+
+  const onSelectVendor = (data) => {
+    setVendor(data)
+    setShowDetail(true)
+  }
+
+  const reset = () => {
+    setVendor('')
+    setShowDetail(false)
+    setVendorList([])
+    setSearchTerm('')
+  }
+
+  const Detail = () => {
+    if(showDetail) {
+      return (
+        <div>
+          <p>Vendor Name: { vendor['vendorName'] }</p>
+          <p>Payment Term: { vendor['paymentTermName'] }</p>
+          <div onClick={() => { reset() }}>Reset</div> 
+        </div>
+      )
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="text" placeholder="seach..." onChange={e=>setSearchTerm(e.target.value)} />
+      {
+        vendorList?.map((v, i) => (
+          <div onClick={() => { onSelectVendor(v) }}>{ v.vendorName } { v.paymentTermName }</div>
+        ))
+      }
+      { Detail() }
     </div>
   );
 }
